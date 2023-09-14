4e15ed12b538ebb7560c763b39c5e021548c2165
Jabsolu <Johnson.absolu@outlook.com>
2023-09-14 21:43:35 +0000

    First commit

--- twodlists.py
+++ twodlists.py
@@ -2,11 +2,11 @@ import random
 
 myList = [[1,2,3,4], [9,8,7,6], [5,8,2,4]]
 
-# for i in range(len(myList)):
-#   value = myList[i]
-#   print(value)
-#   for j in range(len(value)):
-#     print(value[j])
+for i in range(len(myList)):
+  value = myList[i]
+  print(value)
+  for j in range(len(value)):
+    print(value[j])
 
 print(f'Orginal list: {myList}')
 
